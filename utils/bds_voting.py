ae02723673ee1b9e01c43e4c6b5955d7e7de25ce
Yumeng Liu <lym29@mail.ustc.edu.cn>
2019-09-05 22:26:45 +0800

    Modify patch match

--- utils/bds_voting.py
+++ utils/bds_voting.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 import pyflann
 import torch.nn as nn
 
@@ -25,53 +26,58 @@ def modify_patch_minmax(sy, sx, ry, rx, s_patch_min, s_patch_max, r_patch_min, r
 
 
 class PatchMatch(nn.Module):
-    def __init__(self, a, b, patch_size=3):
+    def __init__(self, s, t, patch_size=3):
         super(PatchMatch, self).__init__()
-        self.source_map = a
-        self.target_map = b
+        self.source_map = s
+        self.target_map = t
         self.f_dim = self.source_map.shape[2]
         self.patch_size = patch_size
 
-        self.nnf = np.zeros((a.shape[0], a.shape[1], 2)).astype(np.int)  # The NNF
-        self.nnd = np.zeros(a.shape)  # The NNF distance map
+        n, c, h, w = s.size()
 
-    def build_feat_for_flann(self, patch_center, feature_map):
-        map_2d_size = np.asarray(feature_map.shape[:2])
+        self.batch_size = n
+        self.nnf = torch.zeros((n, 2, h, w), dtype=int)  # The NNF
+        self.nnd = torch.zeros(n, 1, h, w)  # The NNF distance map
 
-        patch_min, patch_max = get_patch_minmax(map_2d_size, patch_center, self.patch_size)
+    def build_pts_set_for_flann(self, x):
+        patch_size = self.patch_size
+        n, c, h, w = x.size()
+        step = patch_size // 2
+        y = torch.zeros((n, c, h + 2 * step, w + 2 * step))
+        y[:, :, step:step + h, step:step + w] = x
+        y[:, :, step:step + h, :step] = x[:, :, :, 0:1].expand((n, c, h, step))
+        y[:, :, step:step + h, step + w:step + 2 * w] = x[:, :, :, -1:w + 2 * step].expand((n, c, h, step))
+        y[:, :, :step, :] = y[:, :, step:step + 1, :].expand((n, c, step, w + 2 * step))
+        y[:, :, h + step:, :] = y[:, :, h + step - 1:h + step, :].expand((n, c, step, w + 2 * step))
 
-        patch = feature_map[patch_min[0]:patch_max[0], patch_min[1]:patch_max[1], :]
-        patch_array = patch.reshape((-1, self.f_dim))
-        output = np.zeros((self.patch_size * self.patch_size, self.f_dim))
+        # build feat tensor for flann
+        ex_feat = torch.zeros(n, c, patch_size * patch_size, h, w)  # extended feature map
+        for i in range(patch_size):
+            for j in range(patch_size):
+                pn = i * patch_size + j
+                ex_feat[:, :, pn, :, :] = y[:, :, i:i + h, j:j + w]
 
-        output[:patch_array.shape[0], :] = patch_array
+        temp = ex_feat.reshape((n, c * patch_size * patch_size, h, w))
+        point_set = temp.reshape((n, c * patch_size * patch_size, h * w))
 
-        return output.reshape((1, -1))
-
-    def build_dataset_for_flann(self, feature_map):
-        dataset_num = feature_map.shape[0] * feature_map.shape[1]
-        dataset = np.zeros((dataset_num, self.patch_size * self.patch_size * self.f_dim))
-        count = 0
-        for i in range(feature_map.shape[0]):
-            for j in range(feature_map.shape[1]):
-                feat = self.build_feat_for_flann(np.array([i, j]), feature_map)
-                dataset[count, :] = feat
-                count += 1
-        return dataset
+        return point_set
 
     def find_nnf(self):
         flann = pyflann.FLANN()
-        pts = self.build_dataset_for_flann(self.target_map)
-        q_pts = self.build_dataset_for_flann(self.source_map)
-        result_id, dists = flann.nn(pts, q_pts, 1, algorithm='kdtree', trees=4)
-        count = 0
-        for i in range(self.source_map.shape[0]):
-            for j in range(self.source_map.shape[1]):
-                id_in_targ = result_id[count]
-                idy, idx = id_in_targ // self.target_map.shape[1], id_in_targ % self.target_map.shape[1]
-                self.nnf[i, j, :] = np.array([idy, idx])
-                self.nnd[i, j, :] = dists[count]
-                count += 1
+        pts_tensor = self.build_pts_set_for_flann(self.target_map)
+        q_pts_tensor = self.build_pts_set_for_flann(self.source_map)
+        for n in range(self.batch_size):
+            pts = pts_tensor[n, :, :].numpy().transpose(1, 0)
+            q_pts = q_pts_tensor[n, :, :].numpy().transpose(1, 0)
+            result_id, dists = flann.nn(pts, q_pts, 1, algorithm='kdtree', trees=4)
+            count = 0
+            for i in range(self.source_map.shape[0]):
+                for j in range(self.source_map.shape[1]):
+                    id_in_targ = result_id[count]
+                    idy, idx = id_in_targ // self.target_map.shape[1], id_in_targ % self.target_map.shape[1]
+                    self.nnf[n, i, j, :] = np.array([idy, idx])
+                    self.nnd[n, i, j, :] = dists[count]
+                    count += 1
 
     def forward(self):
         self.find_nnf()
