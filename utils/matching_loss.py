5db616832c5caccf3640a12a6ddbf3e5cd48fc46
Yumeng Liu <lym29@mail.ustc.edu.cn>
2019-09-11 15:30:52 +0800

    add matching loss

--- utils/matching_loss.py
+++ utils/matching_loss.py
@@ -1,4 +1,9 @@
 import torch
 
+
 def compute_matching_loss(p1, p2, f1, f2):
-    return
+    weight = torch.norm(f1 - f2, dim=1)
+    weight = 1 - torch.pow(weight, 2)/2
+    loss = torch.norm(p1 - p2, dim=1)
+    loss = weight * loss
+    return torch.mean(loss)
